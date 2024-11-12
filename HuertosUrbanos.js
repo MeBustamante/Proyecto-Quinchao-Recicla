0ecfa471a0d5c96a0f2164601258597ad9b0536e
NsShadowJR <matiaseduardo.llaipen@alumnos.ulagos.cl>
2024-11-12 05:45:08 -0300

    modificando huertosurbanos

--- HuertosUrbanos.js
+++ HuertosUrbanos.js
@@ -23,7 +23,7 @@ const HuertosUrbanos = ({ navigation }) => {
                     </Text>
                     <Image source={require('./assets/Huertosurbanos.png')} style={styles.image} />
                     <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-                        <Text style={styles.backButtonText}>Volver al inicio</Text>
+                        <Text style={styles.backButtonText}>Volver</Text>
                     </TouchableOpacity>
                 </View>
             </LinearGradient>
