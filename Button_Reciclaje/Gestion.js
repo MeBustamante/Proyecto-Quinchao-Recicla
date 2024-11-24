7a6130c5921bd5417bd48f9c8d75c97c238bcf01
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-24 00:47:27 -0300

    cambios

--- Button_Reciclaje/Gestion.js
+++ Button_Reciclaje/Gestion.js
@@ -168,7 +168,7 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
   banner: {
-    width: '95%',
+    width: '100%',
     height: 95,
     resizeMode: 'cover',
     marginBottom: 20,
--- Menu_Inferior/Acerca.js
+++ Menu_Inferior/Acerca.js
@@ -1,6 +1,6 @@
 // Acerca de ...
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';  
 import { FontAwesome } from '@expo/vector-icons';  
@@ -11,7 +11,7 @@ import { AppContext } from '../ConfigGlobal/AppContext';
 
 export default function App() {
   // Obtén el idioma actual desde el contexto
-  const { language, toggleLanguage } = useContext(AppContext);
+  const { language } = useContext(AppContext);
 
   // Funciones para abrir los enlaces
   const openInstagram = () => {
@@ -25,20 +25,20 @@ export default function App() {
   // Textos en español e inglés
   const texts = {
     es: {
-      welcome: '¡¡Bienvenid@s a Quinchao Recicla!!',
       team: 'El equipo de desarrollo está compuesto por Kathya Mansilla, Marcelo Bustamante, Yeremi Aburto, Matías Llaipen y Manuel Sanhueza.',
       thanks: '¡Gracias por elegir nuestra app y unirte al cuidado del planeta! Juntos podemos hacer la diferencia. ¡Disfruta y sigue contribuyendo al cambio!',
       follow: '¡Síguenos en Redes Sociales!',
       instagramText: '@Muniquinchao',
       facebookText: 'Municipalidad Quinchao',
+      secondMessage: 'Esta aplicación móvil nació como una iniciativa de la Ilustre Municipalidad de Quinchao, con el objetivo de incentivar el reciclaje y promover la gestión sostenible de residuos en la comunidad.\n\nEl desarrollo de la APP estuvo a cargo de un talentoso equipo de estudiantes de Ingeniería en Informática de la Universidad de Los Lagos, sede Chiloé, quienes con dedicación y creatividad hicieron realidad esta idea:\n\nLíder del Proyecto: Marcelo Bustamante Villarroel\n\nProgramadores y Diseñadores: Matías Llaipén Uribe, Kathya Mansilla Fernández, Manuel Sanhueza Villanueva, Yeremi Aburto Miranda\n\nEsta herramienta es un paso hacia un futuro más limpio y sustentable. ¡Gracias por ser parte del cambio!\n\nCastro, Chiloé, 2024',
     },
     en: {
-      welcome: 'Welcome to Quinchao Recicla!!',
       team: 'The development team is composed of Kathya Mansilla, Marcelo Bustamante, Yeremi Aburto, Matías Llaipen, and Manuel Sanhueza.',
       thanks: 'Thank you for choosing our app and joining the care of the planet! Together we can make a difference. Enjoy and keep contributing to the change!',
       follow: 'Follow us on Social Media!',
       instagramText: '@Muniquinchao',
       facebookText: 'Municipality of Quinchao',
+      secondMessage: 'This mobile app was born as an initiative of the Illustrious Municipality of Quinchao, with the goal of encouraging recycling and promoting sustainable waste management in the community.\n\nThe development of the app was carried out by a talented team of Computer Science students from the University of Los Lagos, Chiloé campus, who with dedication and creativity made this idea a reality:\n\nProject Leader: Marcelo Bustamante Villarroel\n\nProgrammers and Designers: Matías Llaipén Uribe, Kathya Mansilla Fernández, Manuel Sanhueza Villanueva, Yeremi Aburto Miranda\n\nThis tool is a step towards a cleaner and more sustainable future. Thank you for being part of the change!\n\nCastro, Chiloé, 2024',
     }
   };
 
@@ -50,44 +50,48 @@ export default function App() {
       {/* Apply the LinearGradient as background */}
       <LinearGradient colors={['#A8E6CF', '#DCEDC1', '#FFF9C4', '#FFD54F']} style={styles.gradientBackground}>
         
-        <View style={styles.imageContainer}>
-          <Image 
-            source={require('../assets/LOG_AMBIENTE.jpg')}  
-            style={styles.rightImage}
-          />
-        </View>
-
-        <View style={styles.infoContainer}>
-          <Text style={styles.infoText}>
-            {currentTexts.welcome}{'\n'}
-            {currentTexts.team}{'\n\n'}
-            {currentTexts.thanks}
-          </Text>
-          
-          <Image
-            source={require('../assets/XD.jpg')}  // Path to the new image
-            style={styles.newImage}  // Use the updated style
-          />
-
-          <Text style={styles.infoText}>
-            {currentTexts.follow}
-          </Text>
-          {/* Container for social media */}
-          <View style={styles.footerContainer}>
-            {/* Instagram link */}
-            <TouchableOpacity onPress={openInstagram} style={styles.socialIconContainer}>
-              <Ionicons name="logo-instagram" size={29} color="#833AB4" style={styles.icon} />
-              <Text style={styles.footerText}>{currentTexts.instagramText}</Text>
-            </TouchableOpacity>
-
-            {/* Facebook link */}
-            <TouchableOpacity onPress={openFacebook} style={styles.socialIconContainer}>
-              <FontAwesome name="facebook" size={29} color="#3b5998" style={styles.icon} />
-              <Text style={styles.footerText}>{currentTexts.facebookText}</Text>
-            </TouchableOpacity>
+        {/* ScrollView para contenido desplazable */}
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+          <View style={styles.imageContainer}>
+            <Image 
+              source={require('../assets/LOG_AMBIENTE.jpg')}  
+              style={styles.rightImage}
+            />
           </View>
-        </View>
 
+          <View style={styles.infoContainer}>
+            {/* Ya no mostramos el texto de bienvenida */}
+            
+            {/* Segundo texto con la nueva información */}
+            <Text style={styles.infoText}>
+              {currentTexts.secondMessage}
+            </Text>
+
+            <Image
+              source={require('../assets/XD.jpg')}  
+              style={styles.newImage}  
+            />
+
+            <Text style={styles.infoText}>
+              {currentTexts.follow}
+            </Text>
+            
+            {/* Contenedor para los iconos de redes sociales */}
+            <View style={styles.footerContainer}>
+              {/* Instagram link */}
+              <TouchableOpacity onPress={openInstagram} style={styles.socialIconContainer}>
+                <Ionicons name="logo-instagram" size={29} color="#833AB4" style={styles.icon} />
+                <Text style={styles.footerText}>{currentTexts.instagramText}</Text>
+              </TouchableOpacity>
+
+              {/* Facebook link */}
+              <TouchableOpacity onPress={openFacebook} style={styles.socialIconContainer}>
+                <FontAwesome name="facebook" size={29} color="#3b5998" style={styles.icon} />
+                <Text style={styles.footerText}>{currentTexts.facebookText}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
       </LinearGradient>
 
       <MenuInferior />  
@@ -108,41 +112,47 @@ const styles = StyleSheet.create({
     width: '100%',  
     alignItems: 'center', 
     justifyContent: 'flex-start',  
-    padding: 5,  // Increased padding to avoid being too compressed
+    padding: 1,  
   },
-  
+  scrollViewContainer: {
+    flexGrow: 1,  // Permite que el contenido crezca si es necesario
+    alignItems: 'center',  // Centra el contenido dentro del ScrollView
+    paddingBottom: 80,  // Aumentamos el paddingBottom para que no se oculten los botones
+  },
   imageContainer: {
     width: '100%',
     flexDirection: 'row', 
     justifyContent: 'center',  
-    marginBottom: 15, 
+    marginBottom: 10, 
   },
   rightImage: {
-    width: 95,   // Increased image size a bit
+    width: 95,   
     height: 95,
-    borderRadius: 10,  // Added rounded borders
+    borderRadius: 10,  
     borderWidth: 2,
     borderColor: '#ccc',
   },
   newImage: {
-    width: 290,   // Increased image size to 150px
-    height: 120,  // Increased image size to 150px
-    borderRadius: 15,  // Rounded borders
-    borderWidth: 2,
-    borderColor: '#ccc',
-    marginBottom: 5,  // Margin for separation
+    width: 250,   
+    height: 150,  
+    borderRadius: 15,  
+    borderWidth: 0,
+    borderColor: '#',
+    marginBottom: 5,  
   },
   infoContainer: {
     width: '90%',
     padding: 15,
-    backgroundColor: '#F0FFFF',  
+    backgroundColor: '#ffff',  
     borderRadius: 15,
     marginBottom: 20, 
-    shadowColor: '#000',  // Added shadow for an elevated effect
+    shadowColor: '#000',  
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 10,
   },
+  // Ya no tenemos el estilo para el texto de bienvenida
+  // Estilo para los demás textos
   infoText: {
     fontSize: 18,
     color: '#333',
@@ -154,17 +164,18 @@ const styles = StyleSheet.create({
     flexDirection: 'column',  
     alignItems: 'flex-start',  
     marginTop: 15,  
+    padding: 15,  // Añadir padding para asegurar que no se corten los botones
   },
   socialIconContainer: {
     flexDirection: 'row',  
     alignItems: 'center',  
-    marginBottom: 10,  // Increased margin for more separation
+    marginBottom: 17,  
   },
   icon: {
-    marginRight: 9,  // More space between icon and text
+    marginRight: 9,  
   },
   footerText: {
-    fontSize: 17,
+    fontSize: 18,
     color: '#333',
     textAlign: 'left',  
   },
