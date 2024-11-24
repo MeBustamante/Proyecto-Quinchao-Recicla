9aa0e44c4da1f6e7901ae75113a8256783bcb95a
NsShadowJR <matiaseduardo.llaipen@alumnos.ulagos.cl>
2024-11-24 07:09:42 -0300

    menu de reciclaje ya arreglado

--- Button_Reciclaje/MenuReciclaje.js
+++ Button_Reciclaje/MenuReciclaje.js
@@ -1,33 +1,68 @@
-// MenuReciclaje.js
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, Animated, Easing } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Animated, Easing, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 import MenuInferior from '../Menu_Inferior/MenuInferior';
+import { AppContext } from '../ConfigGlobal/AppContext'; // Importa el contexto
 
 const MenuReciclaje = ({ navigation }) => {
+  const { language } = useContext(AppContext); // Obtener el idioma actual
+
+  const [compostajeHeight] = useState(new Animated.Value(0));
+  const [campanasHeight] = useState(new Animated.Value(0));
   const [showCompostajeOptions, setShowCompostajeOptions] = useState(false);
   const [showCampanasOptions, setShowCampanasOptions] = useState(false);
-  const compostajeOpacity = useState(new Animated.Value(0))[0];
-  const campanasOpacity = useState(new Animated.Value(0))[0];
+
+  const texts = {
+    es: {
+      compostaje: 'Programas de Compostaje',
+      compostajeCasa: 'Compostaje en Casa',
+      compostajeComunidad: 'Compostaje en Comunidad',
+      campanas: 'Campañas de Reciclaje',
+      huertos: 'Huertos Urbanos',
+      reforestacion: 'Reforestación Urbana',
+      gestion: 'Gestión de Residuos',
+    },
+    en: {
+      compostaje: 'Composting Programs',
+      compostajeCasa: 'Home Composting',
+      compostajeComunidad: 'Community Composting',
+      campanas: 'Recycling Campaigns',
+      huertos: 'Urban Gardens',
+      reforestacion: 'Urban Reforestation',
+      gestion: 'Waste Management',
+    },
+  };
 
   const toggleCompostajeOptions = () => {
+    if (!showCompostajeOptions) {
+      Animated.timing(campanasHeight, { toValue: 0, duration: 300, easing: Easing.ease, useNativeDriver: false }).start();
+      setShowCampanasOptions(false);
+    }
+
+    const toValue = showCompostajeOptions ? 0 : 150;
     setShowCompostajeOptions(!showCompostajeOptions);
-    Animated.timing(compostajeOpacity, {
-      toValue: showCompostajeOptions ? 0 : 1,
+    Animated.timing(compostajeHeight, {
+      toValue,
       duration: 300,
       easing: Easing.ease,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
   };
 
   const toggleCampanasOptions = () => {
+    if (!showCampanasOptions) {
+      Animated.timing(compostajeHeight, { toValue: 0, duration: 300, easing: Easing.ease, useNativeDriver: false }).start();
+      setShowCompostajeOptions(false);
+    }
+
+    const toValue = showCampanasOptions ? 0 : 150;
     setShowCampanasOptions(!showCampanasOptions);
-    Animated.timing(campanasOpacity, {
-      toValue: showCampanasOptions ? 0 : 1,
+    Animated.timing(campanasHeight, {
+      toValue,
       duration: 300,
       easing: Easing.ease,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
   };
 
@@ -37,114 +72,94 @@ const MenuReciclaje = ({ navigation }) => {
         
         <Image source={require('../assets/reciclaje_encabezado.png')} style={styles.headerImage} />
 
-        <View style={styles.buttonsContainer}>
-          {/* Botón para Programas de Compostaje */}
-          <TouchableOpacity
-            style={[styles.programBox, styles.compostajeBorder]}
-            onPress={toggleCompostajeOptions}
-            activeOpacity={0.8}
-          >
-            <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
-              <Ionicons name="leaf-outline" size={24} color="white" />
-              <Text style={styles.buttonText}>Programas de Compostaje</Text>
-            </LinearGradient>
-          </TouchableOpacity>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          <View style={styles.buttonsContainer}>
+            {/* Botón para Programas de Compostaje */}
+            <TouchableOpacity
+              style={styles.programBox}
+              onPress={toggleCompostajeOptions}
+              activeOpacity={0.8}
+            >
+              <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
+                <Ionicons name="leaf-outline" size={24} color="white" />
+                <Text style={styles.buttonText}>{texts[language].compostaje}</Text>
+              </LinearGradient>
+            </TouchableOpacity>
 
-          {showCompostajeOptions && (
-            <Animated.View style={{ opacity: compostajeOpacity, alignItems: 'center' }}>
+            <Animated.View style={[styles.dropdown, { height: compostajeHeight }]}>
               <TouchableOpacity
                 style={styles.subOptionBox}
                 onPress={() => navigation.navigate('CompostajeCasa')}
               >
-                <LinearGradient colors={['#FF8C00', '#FFA500']} style={styles.innerButtonGradient}>
-                  <Ionicons name="home-outline" size={20} color="white" />
-                  <Text style={styles.subButtonText}>Compostaje en Casa</Text>
-                </LinearGradient>
+                <Text style={styles.subButtonText}>{texts[language].compostajeCasa}</Text>
               </TouchableOpacity>
-              
               <TouchableOpacity
                 style={styles.subOptionBox}
                 onPress={() => navigation.navigate('CompostajeComunidad')}
               >
-                <LinearGradient colors={['#FF8C00', '#FFA500']} style={styles.innerButtonGradient}>
-                  <Ionicons name="people-outline" size={20} color="white" />
-                  <Text style={styles.subButtonText}>Compostaje en Comunidad</Text>
-                </LinearGradient>
+                <Text style={styles.subButtonText}>{texts[language].compostajeComunidad}</Text>
               </TouchableOpacity>
             </Animated.View>
-          )}
 
-          {/* Botón para Campañas de Reciclaje */}
-          <TouchableOpacity
-            style={[styles.programBox, styles.campanasBorder]}
-            onPress={toggleCampanasOptions}
-            activeOpacity={0.8}
-          >
-            <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
-              <Ionicons name="megaphone-outline" size={24} color="white" />
-              <Text style={styles.buttonText}>Campañas de Reciclaje</Text>
-            </LinearGradient>
-          </TouchableOpacity>
+            {/* Botón para Campañas de Reciclaje */}
+            <TouchableOpacity
+              style={styles.programBox}
+              onPress={toggleCampanasOptions}
+              activeOpacity={0.8}
+            >
+              <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
+                <Ionicons name="megaphone-outline" size={24} color="white" />
+                <Text style={styles.buttonText}>{texts[language].campanas}</Text>
+              </LinearGradient>
+            </TouchableOpacity>
 
-          {showCampanasOptions && (
-            <Animated.View style={{ opacity: campanasOpacity, alignItems: 'center' }}>
+            <Animated.View style={[styles.dropdown, { height: campanasHeight }]}>
               <TouchableOpacity
                 style={styles.subOptionBox}
                 onPress={() => navigation.navigate('HuertosUrbanos')}
               >
-                <LinearGradient colors={['#FF8C00', '#FFA500']} style={styles.innerButtonGradient}>
-                  <Ionicons name="flower-outline" size={20} color="white" />
-                  <Text style={styles.subButtonText}>Huertos Urbanos</Text>
-                </LinearGradient>
+                <Text style={styles.subButtonText}>{texts[language].huertos}</Text>
               </TouchableOpacity>
-              
               <TouchableOpacity
                 style={styles.subOptionBox}
                 onPress={() => navigation.navigate('ReforestacionUrbana')}
               >
-                <LinearGradient colors={['#FF8C00', '#FFA500']} style={styles.innerButtonGradient}>
-                  <Ionicons name="leaf-outline" size={20} color="white" />
-                  <Text style={styles.subButtonText}>Reforestación Urbana</Text>
-                </LinearGradient>
+                <Text style={styles.subButtonText}>{texts[language].reforestacion}</Text>
               </TouchableOpacity>
             </Animated.View>
-          )}
 
-          {/* Tercer botón - Gestión Residuos */}
-          <TouchableOpacity
-            style={[styles.programBox, styles.gestionBorder]}
-            onPress={() => navigation.navigate('Gestion')}
-            activeOpacity={0.8}
-          >
-            <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
-              <Ionicons name="trash-bin-outline" size={24} color="white" />
-              <Text style={styles.buttonText}>Gestión de Residuos</Text>
-            </LinearGradient>
-          </TouchableOpacity>
-        </View>
+            {/* Tercer botón - Gestión Residuos */}
+            <TouchableOpacity
+              style={styles.programBox}
+              onPress={() => navigation.navigate('Gestion')}
+              activeOpacity={0.8}
+            >
+              <LinearGradient colors={['#6DD5FA', '#2980B9']} style={styles.buttonGradient}>
+                <Ionicons name="trash-bin-outline" size={24} color="white" />
+                <Text style={styles.buttonText}>{texts[language].gestion}</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <MenuInferior />
       </LinearGradient>
-
-      <MenuInferior />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center' },
-  gradientBackground: { flex: 1, width: '100%', alignItems: 'center', paddingVertical: 20 },
+  container: { flex: 1 },
+  gradientBackground: { flex: 1, width: '100%' },
   headerImage: { 
     width: '100%', 
     height: 130, 
     resizeMode: 'cover', 
-    marginBottom: 10,
     position: 'absolute',
     top: 0,
   },
-  buttonsContainer: { 
-    width: '80%', 
-    marginTop: 170, // Ajustamos el margen superior para que los botones no se superpongan con la imagen
-  },
-
+  scrollContent: { paddingTop: 170, alignItems: 'center' },
+  buttonsContainer: { width: '80%', marginBottom: 20 },
   programBox: {
     borderRadius: 25,
     marginBottom: 15,
@@ -155,21 +170,6 @@ const styles = StyleSheet.create({
     shadowRadius: 6,
     elevation: 5,
   },
-
-  // Estilos de bordes personalizados
-  compostajeBorder: {
-    borderWidth: 2,
-    borderColor: '#FFEB3B', // Amarillo
-  },
-  campanasBorder: {
-    borderWidth: 2,
-    borderColor: '#FF9800', // Naranja
-  },
-  gestionBorder: {
-    borderWidth: 2,
-    borderColor: '#4CAF50', // Verde
-  },
-
   buttonGradient: {
     paddingVertical: 20,
     paddingHorizontal: 30,
@@ -178,29 +178,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
   },
+  dropdown: {
+    overflow: 'hidden',
+  },
   subOptionBox: {
-    width: '90%',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FF9800',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
     borderRadius: 20,
     marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5,
-  },
-  innerButtonGradient: {
-    paddingVertical: 15,
-    paddingHorizontal: 25,
-    borderRadius: 20,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
   },
   buttonText: { fontSize: 18, color: 'white', fontWeight: 'bold', marginLeft: 10 },
-  subButtonText: { fontSize: 16, color: 'white', fontWeight: 'bold', marginLeft: 10 },
+  subButtonText: { fontSize: 16, color: 'white', fontWeight: 'bold' },
 });
 
 export default MenuReciclaje;
