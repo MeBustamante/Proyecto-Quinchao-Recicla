15c6609a4df6e020ac5608e58351a16083e993ac
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-23 23:34:32 -0300

    traduciones

--- Button_Reciclaje/Gestion.js
+++ Button_Reciclaje/Gestion.js
@@ -1,21 +1,23 @@
 // Gestión Residuos
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View, Alert, Image, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { LinearGradient } from 'expo-linear-gradient';
 import MenuInferior from '../Menu_Inferior/MenuInferior';
+import { AppContext } from '../ConfigGlobal/AppContext';
 
 const Gestion = () => {
+  const { language } = useContext(AppContext); // Obtenemos el idioma actual
 
   const handleDayPress = (day) => {
     const dateString = day.dateString;
-    
+
     if (fridays[dateString]) {
       const { selectedColor, selectedTextColor, hours, address } = fridays[dateString];
       if (selectedColor === 'red') {
         Alert.alert(
-          'Recolección de residuos',
-          `Hoy es el día que pasamos a recolectar los residuos en tu área.\n\nHora: ${hours}\nDirección: ${address}`,
+          texts[language].alertTitle,
+          `${texts[language].alertMessage}\n\nHora: ${hours}\nDirección: ${address}`,
         );
       }
     }
@@ -23,17 +25,16 @@ const Gestion = () => {
 
   const generateFridays = (year) => {
     const fridays = {};
-    
+
     for (let month = 0; month < 12; month++) {
       const startDate = new Date(year, month, 1);
       const firstDay = startDate.getDay();
 
       // Calcula el primer viernes del mes
       let firstFriday = 1 + (5 - firstDay + 7) % 7;
-      
+
       for (let i = firstFriday; i <= 31; i += 7) {
         const dayString = `${year}-${(month + 1) < 10 ? '0' + (month + 1) : (month + 1)}-${i < 10 ? '0' + i : i}`;
-        
 
         fridays[dayString] = {
           selected: true,
@@ -49,12 +50,42 @@ const Gestion = () => {
         };
       }
     }
-    
+
     return fridays;
   };
 
   const year = 2024;  // Año para generar los viernes
   const fridays = generateFridays(year);
+
+  // Textos en español e inglés
+  const texts = {
+    es: {
+      alertTitle: 'Recolección de residuos',
+      alertMessage: 'Hoy es el día que pasamos a recolectar los residuos en tu área.',
+      referenceText: 'Mantén tu zona limpia y organizada con nuestro calendario de recolección. Aquí podrás ver las fechas programadas:',
+      recourseTitle: 'Recorrido de Recolección',
+      recourseText: 'Aquí tienes el recorrido de recolección para las diferentes zonas:',
+      recourseAddress1: 'Calle Mirador Alto 123',
+      recourseAddress2: 'Calle Serrano 456',
+      recourseAddress3: 'Calle Padre German Ampuero 789',
+      recourseHours1: 'Hora: 10:00 AM a 10:00 AM',
+      recourseHours2: 'Hora: 11:30 AM a 12:00 PM',
+      recourseHours3: 'Hora: 1:00 PM a 2:00 PM',
+    },
+    en: {
+      alertTitle: 'Waste Collection',
+      alertMessage: 'Today is the day we come to collect waste in your area.',
+      referenceText: 'Keep your area clean and organized with our waste collection calendar. Here you can see the scheduled dates:',
+      recourseTitle: 'Collection Route',
+      recourseText: 'Here is the collection route for the different zones:',
+      recourseAddress1: 'Mirador Alto Street 123',
+      recourseAddress2: 'Serrano Street 456',
+      recourseAddress3: 'Padre German Ampuero Street 789',
+      recourseHours1: 'Time: 10:00 AM to 10:00 AM',
+      recourseHours2: 'Time: 11:30 AM to 12:00 PM',
+      recourseHours3: 'Time: 1:00 PM to 2:00 PM',
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -66,42 +97,42 @@ const Gestion = () => {
 
           <View style={styles.secondContainer}>
             <Text style={styles.referenceText}>
-              Mantén tu zona limpia y organizada con nuestro calendario de recolección. Aquí podrás ver las fechas programadas:
+              {texts[language].referenceText}
             </Text>
           </View>
 
           <View style={styles.thirdContainer}>
             <Calendar
               markedDates={fridays}
-              monthFormat={'MMMM yyyy'} 
+              monthFormat={'MMMM yyyy'}
               theme={{
-                selectedDayBackgroundColor: 'red', 
-                todayTextColor: 'black', 
-                arrowColor: 'black', 
-                dayTextColor: '#000', 
+                selectedDayBackgroundColor: 'red',
+                todayTextColor: 'black',
+                arrowColor: 'black',
+                dayTextColor: '#000',
               }}
               onDayPress={handleDayPress}
-              locale={'es'} 
+              locale={language === 'es' ? 'es' : 'en'} // Cambia el idioma del calendario
             />
           </View>
 
           {/* Cuadro de Recorrido */}
           <View style={styles.recourseContainer}>
-            <Text style={styles.recourseTitle}>Recorrido de Recolección</Text>
+            <Text style={styles.recourseTitle}>{texts[language].recourseTitle}</Text>
             <Text style={styles.recourseText}>
-              Aquí tienes el recorrido de recolección para las diferentes zonas:
+              {texts[language].recourseText}
             </Text>
             <View style={styles.recourseItem}>
-              <Text style={styles.recourseAddress}>Calle Mirador Alto 123</Text>
-              <Text style={styles.recourseHours}>Hora: 10:00 AM a 10:00 AM</Text>
+              <Text style={styles.recourseAddress}>{texts[language].recourseAddress1}</Text>
+              <Text style={styles.recourseHours}>{texts[language].recourseHours1}</Text>
             </View>
             <View style={styles.recourseItem}>
-              <Text style={styles.recourseAddress}>Calle Serrano 456</Text>
-              <Text style={styles.recourseHours}>Hora: 11:30 AM a 12:00 PM</Text>
+              <Text style={styles.recourseAddress}>{texts[language].recourseAddress2}</Text>
+              <Text style={styles.recourseHours}>{texts[language].recourseHours2}</Text>
             </View>
             <View style={styles.recourseItem}>
-              <Text style={styles.recourseAddress}>Calle Padre German Ampuero 789</Text>
-              <Text style={styles.recourseHours}>Hora: 1:00 PM a 2:00 PM</Text>
+              <Text style={styles.recourseAddress}>{texts[language].recourseAddress3}</Text>
+              <Text style={styles.recourseHours}>{texts[language].recourseHours3}</Text>
             </View>
           </View>
         </ScrollView>
@@ -119,14 +150,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'transparent',
-    justifyContent: 'center', // Centra todo el contenido en el eje vertical
-    alignItems: 'center', // Centra todo el contenido en el eje horizontal
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   scrollContainer: {
     flexGrow: 1,
-    paddingBottom: 80, // Asegúrate de que haya espacio para el menú inferior
-    justifyContent: 'center', // Centra el contenido del ScrollView
-    alignItems: 'center', // Centra el contenido horizontalmente
+    paddingBottom: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   gradientBackground: {
     flex: 1,
@@ -134,16 +165,16 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     paddingTop: 10,
-    paddingBottom: 20, // Agregado padding bottom para asegurar que el fondo cubra hasta el fondo de la pantalla
+    paddingBottom: 20,
   },
   banner: {
     width: '95%',
     height: 95,
     resizeMode: 'cover',
-    marginBottom: 20, // Separación entre el banner y el siguiente contenido
+    marginBottom: 20,
   },
   secondContainer: {
-    marginBottom: 20, // Separación entre los textos
+    marginBottom: 20,
     paddingHorizontal: 20,
     width: '100%',
     alignItems: 'center',
@@ -163,7 +194,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: 350,
     alignItems: 'center',
-    marginBottom: 10, // Separación entre el calendario y el siguiente contenido
+    marginBottom: 10,
   },
   recourseContainer: {
     marginTop: 5,
@@ -174,7 +205,7 @@ const styles = StyleSheet.create({
     borderWidth: 2.5,
     borderColor: '#4CAF50',
     alignItems: 'center',
-    marginBottom: 10, // Separación entre el cuadro de recorrido y el menú inferior
+    marginBottom: 10,
   },
   recourseTitle: {
     fontSize: 18,
@@ -211,7 +242,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     width: '100%',
-    zIndex: 10, // Asegura que el menú esté encima del contenido
+    zIndex: 10,
   },
 });
 
--- Menu_Inferior/Acerca.js
+++ Menu_Inferior/Acerca.js
@@ -1,14 +1,18 @@
 // Acerca de ...
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';  
 import { FontAwesome } from '@expo/vector-icons';  
 import MenuInferior from '../Menu_Inferior/MenuInferior';
-import { Linking } from 'react-native'; // Importar Linking
-import { LinearGradient } from 'expo-linear-gradient'; // Importamos LinearGradient
+import { Linking } from 'react-native'; // Import Linking
+import { LinearGradient } from 'expo-linear-gradient'; // Import LinearGradient
+import { AppContext } from '../ConfigGlobal/AppContext';
 
 export default function App() {
+  // Obtén el idioma actual desde el contexto
+  const { language, toggleLanguage } = useContext(AppContext);
+
   // Funciones para abrir los enlaces
   const openInstagram = () => {
     Linking.openURL('https://www.instagram.com/muniquinchao/');
@@ -18,9 +22,32 @@ export default function App() {
     Linking.openURL('https://www.facebook.com/MuniQuinchao');
   };
 
+  // Textos en español e inglés
+  const texts = {
+    es: {
+      welcome: '¡¡Bienvenid@s a Quinchao Recicla!!',
+      team: 'El equipo de desarrollo está compuesto por Kathya Mansilla, Marcelo Bustamante, Yeremi Aburto, Matías Llaipen y Manuel Sanhueza.',
+      thanks: '¡Gracias por elegir nuestra app y unirte al cuidado del planeta! Juntos podemos hacer la diferencia. ¡Disfruta y sigue contribuyendo al cambio!',
+      follow: '¡Síguenos en Redes Sociales!',
+      instagramText: '@Muniquinchao',
+      facebookText: 'Municipalidad Quinchao',
+    },
+    en: {
+      welcome: 'Welcome to Quinchao Recicla!!',
+      team: 'The development team is composed of Kathya Mansilla, Marcelo Bustamante, Yeremi Aburto, Matías Llaipen, and Manuel Sanhueza.',
+      thanks: 'Thank you for choosing our app and joining the care of the planet! Together we can make a difference. Enjoy and keep contributing to the change!',
+      follow: 'Follow us on Social Media!',
+      instagramText: '@Muniquinchao',
+      facebookText: 'Municipality of Quinchao',
+    }
+  };
+
+  // Seleccionamos el texto según el idioma
+  const currentTexts = texts[language];
+
   return (
     <View style={styles.container}>
-      {/* Aplicamos el LinearGradient como fondo */}
+      {/* Apply the LinearGradient as background */}
       <LinearGradient colors={['#A8E6CF', '#DCEDC1', '#FFF9C4', '#FFD54F']} style={styles.gradientBackground}>
         
         <View style={styles.imageContainer}>
@@ -32,31 +59,31 @@ export default function App() {
 
         <View style={styles.infoContainer}>
           <Text style={styles.infoText}>
-            ¡¡Bienvenid@s a Quinchao Recicla!!{'\n'}
-            El equipo de desarrollo está compuesto por Kathya Mansilla, Marcelo Bustamante, Yeremi Aburto, Matías Llaipen y Manuel Sanhueza.{'\n\n'}
-            ¡Gracias por elegir nuestra app y unirte al cuidado del planeta! Juntos podemos hacer la diferencia. ¡Disfruta y sigue contribuyendo al cambio!
+            {currentTexts.welcome}{'\n'}
+            {currentTexts.team}{'\n\n'}
+            {currentTexts.thanks}
           </Text>
           
           <Image
-            source={require('../assets/XD.jpg')}  // Ruta de la nueva imagen
-            style={styles.newImage}  // Usamos el estilo actualizado
+            source={require('../assets/XD.jpg')}  // Path to the new image
+            style={styles.newImage}  // Use the updated style
           />
 
           <Text style={styles.infoText}>
-            ¡Síguenos en Redes Sociales!
+            {currentTexts.follow}
           </Text>
-          {/* Contenedor para las redes sociales */}
+          {/* Container for social media */}
           <View style={styles.footerContainer}>
-            {/* Enlace a Instagram */}
+            {/* Instagram link */}
             <TouchableOpacity onPress={openInstagram} style={styles.socialIconContainer}>
               <Ionicons name="logo-instagram" size={29} color="#833AB4" style={styles.icon} />
-              <Text style={styles.footerText}>@Muniquinchao</Text>
+              <Text style={styles.footerText}>{currentTexts.instagramText}</Text>
             </TouchableOpacity>
 
-            {/* Enlace a Facebook */}
+            {/* Facebook link */}
             <TouchableOpacity onPress={openFacebook} style={styles.socialIconContainer}>
               <FontAwesome name="facebook" size={29} color="#3b5998" style={styles.icon} />
-              <Text style={styles.footerText}>Municipalidad Quinchao</Text>
+              <Text style={styles.footerText}>{currentTexts.facebookText}</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -81,7 +108,7 @@ const styles = StyleSheet.create({
     width: '100%',  
     alignItems: 'center', 
     justifyContent: 'flex-start',  
-    padding: 5,  // Aumentamos el padding para que no se vea tan comprimido
+    padding: 5,  // Increased padding to avoid being too compressed
   },
   
   imageContainer: {
@@ -91,19 +118,19 @@ const styles = StyleSheet.create({
     marginBottom: 15, 
   },
   rightImage: {
-    width: 95,   // Aumentamos un poco el tamaño de la imagen
+    width: 95,   // Increased image size a bit
     height: 95,
-    borderRadius: 10,  // Agregamos bordes redondeados
+    borderRadius: 10,  // Added rounded borders
     borderWidth: 2,
     borderColor: '#ccc',
   },
   newImage: {
-    width: 290,   // Aumentamos el tamaño de la imagen a 150px
-    height: 120,  // Aumentamos el tamaño de la imagen a 150px
-    borderRadius: 15,  // Bordes redondeados
+    width: 290,   // Increased image size to 150px
+    height: 120,  // Increased image size to 150px
+    borderRadius: 15,  // Rounded borders
     borderWidth: 2,
     borderColor: '#ccc',
-    marginBottom: 5,  // Margen inferior para separación
+    marginBottom: 5,  // Margin for separation
   },
   infoContainer: {
     width: '90%',
@@ -111,7 +138,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0FFFF',  
     borderRadius: 15,
     marginBottom: 20, 
-    shadowColor: '#000',  // Agregamos sombra para dar un efecto elevado
+    shadowColor: '#000',  // Added shadow for an elevated effect
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 10,
@@ -131,10 +158,10 @@ const styles = StyleSheet.create({
   socialIconContainer: {
     flexDirection: 'row',  
     alignItems: 'center',  
-    marginBottom: 10,  // Aumentamos el margen para mayor separación
+    marginBottom: 10,  // Increased margin for more separation
   },
   icon: {
-    marginRight: 9,  // Más espacio entre el ícono y el texto
+    marginRight: 9,  // More space between icon and text
   },
   footerText: {
     fontSize: 17,
