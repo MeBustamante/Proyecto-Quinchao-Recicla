5b4f7eef76a866471a05c2fc1e60bebbb3a556a2
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-25 00:44:05 -0300

    Menú inferior en Inglés

--- Menu_Inferior/MenuInferior.js
+++ Menu_Inferior/MenuInferior.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { AppContext } from '../ConfigGlobal/AppContext'; // Importa el contexto global
 
 const MenuInferior = () => {
+  const { language } = useContext(AppContext); // Obtén el idioma del contexto
   const navigation = useNavigation(); // Se utiliza useNavigation para manejar la navegación
 
   const goToHome = () => {
     navigation.reset({
       index: 0,
-      routes: [{ name: 'Home' }], 
+      routes: [{ name: 'Home' }],
     });
+  };
+
+  // Textos en español e inglés
+  const texts = {
+    es: {
+      config: 'Configuración',
+      home: 'Inicio',
+      help: 'Ayuda',
+      about: 'Acerca de',
+    },
+    en: {
+      config: 'Settings',
+      home: 'Home',
+      help: 'Help',
+      about: 'About Us',
+    },
   };
 
   return (
     <View style={styles.menuContainer}>
-      <Image 
+      <Image
         source={require('../assets/mascota.png')}
         style={styles.mascotaImage}
       />
@@ -23,31 +41,31 @@ const MenuInferior = () => {
       {/* Navegación a Configuración */}
       <TouchableOpacity
         style={styles.menuButton}
-        onPress={() => navigation.navigate('Configuracion')} // Navega a la pantalla de configuración
+        onPress={() => navigation.navigate('Configuracion')}
       >
         <Ionicons name="settings-outline" size={24} color="white" />
-        <Text style={styles.menuText}>Configuración</Text>
+        <Text style={styles.menuText}>{texts[language].config}</Text>
       </TouchableOpacity>
-      
+
       {/* Navegación a Inicio */}
       <TouchableOpacity style={styles.menuButton} onPress={goToHome}>
         <Ionicons name="home-outline" size={24} color="white" />
-        <Text style={styles.menuText}>Inicio</Text>
+        <Text style={styles.menuText}>{texts[language].home}</Text>
       </TouchableOpacity>
-      
+
       {/* Navegación a Ayuda */}
       <TouchableOpacity
         style={styles.menuButton}
-        onPress={() => navigation.navigate('Ayuda')} // Puedes agregar la pantalla de Ayuda
+        onPress={() => navigation.navigate('Ayuda')}
       >
         <Ionicons name="help-circle-outline" size={24} color="white" />
-        <Text style={styles.menuText}>Ayuda</Text>
+        <Text style={styles.menuText}>{texts[language].help}</Text>
       </TouchableOpacity>
 
       {/* Navegación a Acerca de */}
       <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate('Acerca')}>
         <Ionicons name="information-circle-outline" size={24} color="white" />
-        <Text style={styles.menuText}>Acerca de</Text>
+        <Text style={styles.menuText}>{texts[language].about}</Text>
       </TouchableOpacity>
     </View>
   );
