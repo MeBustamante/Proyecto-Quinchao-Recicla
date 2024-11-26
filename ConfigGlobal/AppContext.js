55e06e60ec787e31798e22ba5baa16605d256e9b
MeBustamante <marcelobustamantevillarroel@gmail.com>
2024-11-26 17:48:48 -0300

    Se agrega notifiacion Push

--- ConfigGlobal/AppContext.js
+++ ConfigGlobal/AppContext.js
@@ -1,4 +1,14 @@
 import React, { createContext, useState } from 'react';
+import * as Notifications from 'expo-notifications';
+
+// Configurar el manejador de notificaciones
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 // Crea el contexto
 export const AppContext = createContext();
@@ -10,12 +20,23 @@ export const AppProvider = ({ children }) => {
   const [collectionSchedule, setCollectionSchedule] = useState([]); // Estado para el cronograma
 
   // Función para agregar una nueva notificación con fecha y hora
-  const addNotification = (message) => {
+  const addNotification = async (message) => {
+    const defaultPushMessage = `✅ Su solicitud ha sido ingresada exitosamente y será gestionada a la brevedad. Gracias por su compromiso con el cuidado del medio ambiente y por ser parte del cambio.`;
     const newNotification = {
       text: message,
       date: new Date().toLocaleString(), // Fecha y hora en formato local
     };
     setNotifications((prevNotifications) => [...prevNotifications, newNotification]);
+
+    // Enviar notificación push local
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Quinchao Recicla',
+        body: `✅ Su solicitud ha sido ingresada exitosamente y será gestionada a la brevedad. Gracias por su compromiso.`,
+        data: { data: message },
+      },
+      trigger: null, // Inmediata
+    });
   };
 
   return (
