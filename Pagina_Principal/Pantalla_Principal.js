60a5c56823fd0f3168f239e62056c38a326d7fdd
NsShadowJR <matiaseduardo.llaipen@alumnos.ulagos.cl>
2024-11-27 17:46:56 -0300

    menu inferioor

--- Pagina_Principal/Pantalla_Principal.js
+++ Pagina_Principal/Pantalla_Principal.js
@@ -216,7 +216,7 @@ const PantallaPrincipalScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 35, // Deja un espacio de 40 píxeles en la parte superior
+        paddingTop: 35, // Deja un espacio en el menu inferior
         backgroundColor: '#ffffff', 
     },
     gradientBackground: {
